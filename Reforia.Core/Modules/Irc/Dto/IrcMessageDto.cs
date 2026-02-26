84677bcdc137c3f2667e6cb4af3e8430e0541a5b
Antoni Grabowski <96348595+angrabo@users.noreply.github.com>
2026-02-26 21:19:49 +0100

    app update

---
+++ Reforia.Core/Modules/Irc/Dto/IrcMessageDto.cs
@@ -0,0 +1,6 @@
+namespace Reforia.Core.Modules.Irc.Dto;
+
+public class IrcMessageDto
+{
+    
+}
--- Reforia.DeviceApp/Utils/IrcSignalRBridge.cs
+++ Reforia.DeviceApp/Utils/IrcSignalRBridge.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.SignalR;
 using Reforia.Core.Modules.Irc;
+using Reforia.Core.Modules.Irc.Dto;
 using Reforia.Core.Modules.Irc.Interfaces;
 using ReforiaBackend.Hubs;
 using Serilog;
@@ -29,10 +30,28 @@ public class IrcSignalRBridge : IIrcConnectionObserver
             // TODD: filter and final format
             if (e.RawMessage.Contains("QUIT") || e.RawMessage.Contains("JOIN"))
                 return;
+
+            Console.WriteLine(e.RawMessage);
             
-            await _hub.Clients
-                .Group(e.ConnectionId)
-                .SendAsync("ircMessage", e.RawMessage);
+            if (e.RawMessage.Contains("PRIVMSG"))
+            {
+                var parts = e.RawMessage.Split(' ', 4);
+                var source = parts[0].TrimStart(':');
+                var target = parts[2];
+                var message = parts[3].TrimStart(':');
+
+                var model = new IrcMessageDto()
+                {
+                    ConnectionId = e.ConnectionId,
+                    ChatId = target.Contains('#') ? target : source.Split('!')[0],
+                    Sender = source.Split('!')[0],
+                    Message = message,
+                    Timestamp = DateTime.UtcNow
+                };
+                await _hub.Clients
+                    .Group(e.ConnectionId)
+                    .SendAsync("ircMessage", model);
+            }
         }
         catch (Exception ex)
         {
