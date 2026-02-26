d69658e04c35569de8381de495375a4e23c9f936
Antoni Grabowski <96348595+angrabo@users.noreply.github.com>
2026-02-26 21:19:54 +0100

    Add additional settings options for user preferences

--- Reforia.Core/Modules/Communication/Functions/GetSettingsFunction.cs
+++ Reforia.Core/Modules/Communication/Functions/GetSettingsFunction.cs
@@ -17,12 +17,22 @@ public class GetSettingsFunction : WebFunction<GetSettingsFunctionResponse>
         var apiToken = await config.Get(EConfigOptions.ApiToken);
         var shouldHighlightMessages = await config.Get(EConfigOptions.UserHighlight) == "True";
         var language = await config.Get(EConfigOptions.Language) ?? "en";
+        var alertOnMention = await config.Get(EConfigOptions.AlertOnMention) == "True";
+        var alertOnKeyword = await config.Get(EConfigOptions.AlertOnKeyword) == "True";
+        var highlightOnKeyword = await config.Get(EConfigOptions.HighlightOnKeyword) == "True";
+        var keywordList = await config.Get(EConfigOptions.KeywordList) ?? "";
+        var showBeatmapBanner = await config.Get(EConfigOptions.ShowBeatmapBanner) == "True";
         
         return new GetSettingsFunctionResponse()
         {
             ApiToken = apiToken ?? "",
             IsUserHighlighted = shouldHighlightMessages,
-            Language = language
+            Language = language,
+            AlertOnKeyword = alertOnKeyword,
+            AlertOnMention =  alertOnMention,
+            HighlightOnKeyword = highlightOnKeyword,
+            KeywordList = keywordList.Split(',').Select(k => k.Trim()).Where(k => !string.IsNullOrWhiteSpace(k)).ToList(),
+            ShowBeatmapBanner = showBeatmapBanner
         };
     }
 }
