71eeae8e3553347ad5b49a0dbd661346da490013
Antoni Grabowski <96348595+angrabo@users.noreply.github.com>
2026-02-26 21:19:49 +0100

    removed service

--- Reforia.Core/Common/Database/Interfaces/IConfigRepositoryService.cs
+++
@@ -1,8 +0,0 @@
-using Reforia.Core.Common.Config;
-
-namespace Reforia.Core.Common.Database.Interfaces;
-
-public interface IConfigRepositoryService : IService<ConfigItem>
-{
-    Task<ConfigItem?> GetByKeyAsync(string key, CancellationToken ct = default);
-}
